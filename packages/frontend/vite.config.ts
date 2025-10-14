e7af4ce5b7260bab4d17e51a2f3d67b70b00ca52
Ericgi231 <ericgi231@gmail.com>
2025-10-14 18:14:39 -0500

    Shared code + docker + build + dev working

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// get __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'shared': path.resolve(__dirname, '../shared/src/index.ts')
+    }
+  },
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
