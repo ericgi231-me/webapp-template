1611f3cd0b39ebd36bf808c83d9f53b59ee6630e
Ericgi231 <ericgi231@gmail.com>
2026-01-13 13:51:42 -0600

    Added dev api proxy

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = Number(env.VITE_DEV_FRONTEND_PORT || 5173);
+  const target = `http://localhost:${env.VITE_DEV_BACKEND_PORT || 3000}`;
+  
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port,
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(new RegExp(`^/api`), ''),
+        },
+      },
+    }
+  }
 })
